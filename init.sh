b21df7854443c0be57d6ccb59661870d79b20be9
fmpi.santos <filipesantosdev@gmail.com>
2025-05-06 12:39:13 +0100

    feat: Add tmux config to ~/.bashrc

--- init.sh
+++ init.sh
@@ -8,5 +8,18 @@ chmod +x ~/.local/bin/tmux-sessionizer
 # Move .tmux.conf to ~/
 mv .tmux.conf ~/
 
+echo '
+if [[ ! -z "$TMUX" && -n "$SSH_CONNECTION" ]]; then
+    tmux attach
+else
+    first_session=$(tmux list-sessions -F "#{session_id}" | head -n 1)
+    if [[ -n "$first_session" ]]; then
+        tmux attach-session -t "$first_session"
+    else
+        tmux new-session -s default
+    fi
+fi
+' >> ~/.bashrc
+
 # Run tmux source-file ~/.tmux.conf
 tmux source-file ~/.tmux.conf
